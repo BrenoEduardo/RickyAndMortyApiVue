d3173d2420e6ca328a285e98cbf9307b98a3d9cb
BrenoEduardo <brenoeducm@hotmail.com>
2023-02-09 21:54:53 -0300

    Criação do projeto e componentes e utilização de Vue Router e Pinia

--- testeVue/src/views/ShowInfoPoke.vue
+++ testeVue/src/views/ShowInfoPoke.vue
@@ -1,21 +1,34 @@
 <script setup>
 import { storeToRefs } from "pinia";
 import { usePokemonStore } from "../stores/pokemonState";
+import { computed } from "vue";
+import router from "@/router";
 
 const useStatePokemon = usePokemonStore();
 const { infoPokemon } = storeToRefs(useStatePokemon);
+const namePokemon = computed(() => {
+  return infoPokemon.value?.data?.name;
+});
+const abilitiesPokemon = computed(() => {
+  return infoPokemon.value?.data?.abilities;
+});
+function backHome() {
+  useStatePokemon.$reset();
+  router.push({ name: "home" });
+}
 // const store = useStore()
 console.log(infoPokemon, "info pokemon");
 </script>
 
 <template>
   <div>
-    <h1>Nome do pokemon: {{ infoPokemon.data.name }}</h1>
+    <h1>Nome do pokemon: {{ namePokemon }}</h1>
     <div>
-      <div v-for="infoPokemon in infoPokemon.data.abilities">
-        <div>{{ infoPokemon.ability.name }}</div>
-        <img src={{infoPokemon.ability.url}} alt="">
+      <div v-for="(pokemon, index) in abilitiesPokemon" :key="index">
+        <div>{{ pokemon.ability.name }}</div>
+        <img src="{{pokemon.ability.url}}" alt="" />
       </div>
     </div>
+    <button @click="backHome()">Voltar</button>
   </div>
 </template>
